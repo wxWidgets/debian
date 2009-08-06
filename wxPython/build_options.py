95af77a370d9f7f9795796a30f3af7e38affe5b0
Ryan Niebur <ryanryan52@gmail.com>
2009-08-05 17:24:17 -0700

    add an install-gtk-py 'meta' target, I have no idea why this worked before or why it stopped working now...but this fixes it

--- wxPython/build_options.py
+++ wxPython/build_options.py
@@ -2,10 +2,10 @@
 UNICODE=1
 UNDEF_NDEBUG=1
 INSTALL_MULTIVERSION=1
-FLAVOUR=""
+FLAVOUR="dbg"
 EP_ADD_OPTS=1
 EP_FULL_VER=0
-WX_CONFIG="/opt/wx/current/bin/wx-config --toolkit=gtk2 --unicode=yes --version=2.8 2>/dev/null "
+WX_CONFIG="wx-config --version=2.8 --toolkit=gtk2 --static=no --unicode --debug"
 WXPORT="gtk2"
 MONOLITHIC=0
 FINAL=0
